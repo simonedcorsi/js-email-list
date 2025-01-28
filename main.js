ab901b0a6a0eb3b688033fbe2db7d320e1972a74
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-28 15:45:03 +0100

    impostazione ciclo for

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 const mails = document.getElementById('lista-mail');
-
-axios.get(' https://flynn.boolean.careers/exercises/api/random/mail')
     
     .then(response => {
      console.log(response.data.response)
+
      //codice da eseguire in caso di successo
+     for(let i = 0; i < 10; i++){
+        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
+        
+        .then(response => {
+           mails.innerHTML = response.data.response
 
-     mails.innerHTML = response.data.response
-
+     })
+     
         //    numero = response.data.response;
-    })
+    }
+
     .catch(error => {
         // codice da eseguire in caso di errore
      console.error(error)
