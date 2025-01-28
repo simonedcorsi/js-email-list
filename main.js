330d1d08d7db2440791fbdf11bbba8eb6ee57476
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-28 16:12:46 +0100

    sistemazione

--- main.js
+++ main.js
@@ -1,21 +1,15 @@
 const mails = document.getElementById('lista-mail');
-    
-    .then(response => {
-     console.log(response.data.response)
+let mail = [];
 
      //codice da eseguire in caso di successo
      for(let i = 0; i < 10; i++){
         fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-        
-        .then(response => {
-           mails.innerHTML = response.data.response
-
-     })
-     
-        //    numero = response.data.response;
-    }
-
-    .catch(error => {
-        // codice da eseguire in caso di errore
-     console.error(error)
+        .then((response) => response.json()) // Funzione anonima per il JSON
+                .then((data) => {
+                    mail.push(data.response);
+    
     })
+.catch(error => {
+
+    console.error(error);
+});
